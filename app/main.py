710368263ccd697c1225e7e25a18739305150858
vamsikrishnap23 <vamsikrishnapullabhotla23@gmail.com>
2025-07-19 12:03:48 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.api.endpoints import router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Gayathri Vasthu Jyothishalayam API",
@@ -7,6 +9,14 @@ app = FastAPI(
 )
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
